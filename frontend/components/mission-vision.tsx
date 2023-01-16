6c661a7d2862af78202e11dde50e08468960c4e8
Petar Hristovski <petarhristovski23@gmail.com>
2023-01-16 20:42:28 +0100

    FTR: SemiFinal Mission and vision compoment

--- frontend/components/mission-vision.tsx
+++ frontend/components/mission-vision.tsx
@@ -4,33 +4,48 @@ import { Flex, Spacer } from '@chakra-ui/react'
 import { Heading } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
+import Bulb from "./icons/Bulb";
 
 const MissionVision = ()=> { 
 
 return(
 
-    <Box bgColor={"primary"} height={"300px"} >
-    
-        <HStack spacing='24px'>
-
-            <Box w='400px' h='40px' color='white'>
-                <image> </image>
-                <Heading size='3xl'> Our Mission</Heading>
-                <Text noOfLines={[1, 2, 3]} textAlign='justify'>
-                    Connecting EECS students and supporting
-                    them in their professional, academic and
-                    personal growth. 
-                </Text>
+    <Box bgColor={"primary"} width={"100%"}>
+        <HStack paddingTop={"100px"} paddingBottom={"100px"} marginLeft={"150px"} marginRight={"150px"}>
+            <Box width={"45%"} color='white'>
+                <HStack width={"100%"}>
+                    <Box width={"15%"}>
+                        <Image padding={3} height="80px" width="80px" borderRadius={100} bgColor="white" src="/images/flag.svg" />
+                    </Box>
+                    
+                    <Box width={"85%"}>
+                        <Heading size='3xl'> Our Mission</Heading>
+                        <Text noOfLines={[1, 2, 3]} textAlign='justify'>
+                            Connecting EECS students and supporting
+                            them in their professional, academic and
+                            personal growth. 
+                        </Text>
+                    </Box>
+                </HStack>                
             </Box>
 
-            <Box w='400px' h='40px' color='white'>
-            <Heading size='3xl'> Our Vision</Heading>
-                <Text noOfLines={[1, 2, 3, 4]} textAlign='justify'>
-                    Empowering EECS students across Europe to
-                    reach their full potencial in their academic,
-                    professional and social lives in a strong and
-                    recognizable network.
-                </Text>
+            <Box width={"10%"}></Box>
+
+            <Box width={"45%"} color='white'>
+                <HStack width={"100%"}>
+                    <Box width={"15%"}>
+                        <Image padding={3} height="80px" width="80px" borderRadius={100} bgColor="white" src="/images/bulb.svg" />
+                    </Box>
+                    <Box width="85%">
+                        <Heading size='3xl'> Our Vision</Heading>
+                        <Text noOfLines={[1, 2, 3, 4]} textAlign='justify'>
+                            Empowering EECS students across Europe to
+                            reach their full potencial in their academic,
+                            professional and social lives in a strong and
+                            recognizable network.
+                        </Text>
+                    </Box>
+                </HStack>
             </Box>
 
         </HStack>
