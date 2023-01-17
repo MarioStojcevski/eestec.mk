2e7b782c27933e16b864a5a7ab3248023f55a533
Vasilaki Gorgioski <vasilakigorgi@gmail.com>
2023-01-17 02:32:57 +0100

    FTR: Added MeetTheBoard,made Statistics responsive

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import Header from "../components/shared/header";
 import LandingPage from "../components/landing-page";
 import Statistics from "../components/statistics";
 import MissionVision from "../components/mission-vision";
+import MeetTheBoard from "../components/meet-the-board";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
 
       <Statistics />
 
+      <MeetTheBoard />
       
     </>
   );
