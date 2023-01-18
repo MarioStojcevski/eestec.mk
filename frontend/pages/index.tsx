40189647093284d2f688a5fbf9218a0347952a5a
Stefan Uzunov <uzunov.stefan777@gmail.com>
2023-01-18 22:30:03 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,11 +2,14 @@ import Statistics from "../components/statistics";
 import MeetTheBoard from "../components/meet-the-board";
 import BlogShowcase from "../components/blog-showcase";
 import MakingMemories from "../components/making-memories";
+import WhyYouShouldJoin from "../components/why-you-should-join";
 
 const Home = () => {
   return (
     <>
       <Statistics />
+      
+      <WhyYouShouldJoin />
 
       <MakingMemories />
 
