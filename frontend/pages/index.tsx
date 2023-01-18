cf7193c5dcdc0ccb109fecd58912b5e1af9de8bc
Petar Hristovski <petarhristovski23@gmail.com>
2023-01-18 21:50:24 +0100

    FTR: sponsorship component created

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,8 @@ import Statistics from "../components/statistics";
 import MeetTheBoard from "../components/meet-the-board";
 import BlogShowcase from "../components/blog-showcase";
 import MakingMemories from "../components/making-memories";
+import MissionVision from "../components/mission-vision";
+import Sponsorship from "../components/sponsorship";
 
 const Home = () => {
   return (
@@ -13,6 +15,8 @@ const Home = () => {
       <BlogShowcase />
 
       <MeetTheBoard />
+      
+      <Sponsorship />
     </>
   );
 };
