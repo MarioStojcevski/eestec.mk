0de75705f5dd60c8d7e5890530c6a1f021ee68fc
Vasilaki Gorgioski <vasilakigorgi@gmail.com>
2023-01-20 13:58:55 +0100

    FTR: Update to login screen

--- frontend/components/login-screen.tsx
+++ frontend/components/login-screen.tsx
@@ -1,23 +1,30 @@
-import { Stack , Text , Button, Center, Link } from "@chakra-ui/react"
+ import { Stack , Text , Button, Center, Link , Image , Heading } from "@chakra-ui/react"
 
 const LoginScreen = () =>{
     
 
     return (
         <>
-            <Center h='100vh' bg ='white'>
+            <Center h='100vh' bg ='red'>
                 <Stack boxShadow='md' bg='white' p='20' rounded='md'>
+                <Image src="/images/eestec-logo.png" maxW='50%' mb='10' mx='auto' />
+                <Heading variant='h4'>Sign in</Heading>
+
+                /** Implement form */
 
                     <Stack justify='center' color='gray.600' spacing='3'>
-                        <Text as='div' textAlign='center'>
-                            <span>Don&lsquo;t have an account? </span>
+                        <Text as='div' textAlign='left'>
+                            <Link color='primary' variant='link'>
+                                Forgot password
+                            </Link>
+                            <br />
+                            <br />
+                            <span>  Don&lsquo;t have an account? </span>
                             <Link color='primary' variant='link'>
                                 Sign up
                             </Link>
                         </Text>
-                        <Link color='primary' variant='link'>
-                            Forgot password
-                        </Link>
+                        
                     </Stack>
 
                 </Stack>
