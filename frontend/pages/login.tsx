39622f718ea688f6299522480c65d1236d3867de
7zip <mane.sto@outlook.com>
2023-01-22 15:05:34 +0100

    FTR: some minor changes

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -4,7 +4,6 @@ import {
   Button,
   Center,
   Link,
-  Image,
   Heading,
   VStack,
   Box,
@@ -16,94 +15,81 @@ import {
 
 const Login = () => {
   return (
-    <>
-      <Center bg="white">
-        <VStack w={{ base: "90%", md: "55%", lg: "40%" }} mb="200">
-          <Center mb="50"></Center>
-          <Box
-            boxShadow="dark-lg"
-            bg="white"
-            border="2px"
-            borderColor="primary"
-            p="6"
-            rounded="lg"
+    <Center bg="white">
+      <VStack w={{ base: "90%", md: "55%", lg: "40%" }} mb="200">
+        <Center mb="50"></Center>
+        <Box
+          boxShadow="dark-lg"
+          bg="white"
+          border="2px"
+          borderColor="primary"
+          p="6"
+          rounded="lg"
+        >
+          <Heading
+            variant={{ base: "h5", md: "h4" }}
+            color={{ base: "primary", md: "primary" }}
+            textAlign="left"
+            ml="24.5%"
+            mb="5"
           >
-            <Image
-              src="/images/eestec-logo.png"
-              alt=""
-              maxW="40%"
-              mt="2"
-              mb="10"
-              mx="auto"
+            Sign in
+          </Heading>
+
+          <FormControl textAlign="left" ml="25%" mb="5">
+            <FormLabel fontSize={{ base: "12px", md: "16px" }}>Email</FormLabel>
+
+            <Input
+              type="email"
+              w="50%"
+              boxShadow="inner"
+              fontSize={{ base: "12px", md: "14px", lg: "16px" }}
             />
 
-            <Heading
-              variant={{ base: "h5", md: "h4" }}
-              color={{ base: "primary", md: "primary" }}
-              textAlign="left"
-              ml="24.5%"
-              mb="5"
-            >
-              Sign in
-            </Heading>
+            <FormHelperText hidden={true}>
+              Please enter your email
+            </FormHelperText>
+          </FormControl>
 
-            <FormControl textAlign="left" ml="25%" mb="5">
-              <FormLabel fontSize={{ base: "12px", md: "16px" }}>
-                Email
-              </FormLabel>
+          <FormControl textAlign="left" ml="25%" mb="10">
+            <FormLabel fontSize={{ base: "12px", md: "16px" }}>
+              Password
+            </FormLabel>
 
-              <Input
-                type="email"
-                w="50%"
-                boxShadow="inner"
-                fontSize={{ base: "12px", md: "14px", lg: "16px" }}
-              />
+            <Input
+              type="password"
+              w="50%"
+              boxShadow="inner"
+              fontSize={{ base: "12px", md: "14px", lg: "16px" }}
+            />
 
-              <FormHelperText hidden={true}>
-                Please enter your email
-              </FormHelperText>
-            </FormControl>
+            <FormHelperText hidden={true}>
+              Please enter your password
+            </FormHelperText>
+          </FormControl>
 
-            <FormControl textAlign="left" ml="25%" mb="10">
-              <FormLabel fontSize={{ base: "12px", md: "16px" }}>
-                Password
-              </FormLabel>
-
-              <Input
-                type="password"
-                w="50%"
-                boxShadow="inner"
-                fontSize={{ base: "12px", md: "14px", lg: "16px" }}
-              />
-
-              <FormHelperText hidden={true}>
-                Please enter your password
-              </FormHelperText>
-            </FormControl>
-
-            <Stack justify="center" spacing="4">
-              <Text as="div" textAlign="left">
-                <Center fontSize={{ base: "10px", md: "14px" }} mb="1.5">
-                  <Link color="primary" variant="link" mr="10" ml="5">
-                    Forgot password
-                  </Link>
-                  <Button mr="5" ml="5">
-                    LOG IN
-                  </Button>
-                </Center>
-                <br />
-                <Center mb="3" fontSize={{ base: "10px", md: "14px" }}>
-                  <span> Don&lsquo;t have an account? &nbsp;</span>
-                  <Link color="primary" variant="link">
-                    Sign up
-                  </Link>
-                </Center>
-              </Text>
-            </Stack>
-          </Box>
-        </VStack>
-      </Center>
-    </>
+          <Stack justify="center" spacing="4">
+            <Text as="div" textAlign="left">
+              <Center fontSize={{ base: "10px", md: "14px" }} mb="1.5">
+                <Link color="primary" variant="link" mr="10" ml="5">
+                  Forgot password
+                </Link>
+                <Button mr="5" ml="5">
+                  LOG IN
+                </Button>
+              </Center>
+              <br />
+              <Center mb="3" fontSize={{ base: "10px", md: "14px" }}>
+                <span> Don&lsquo;t have an account? &nbsp;</span>
+                <Link color="primary" variant="link">
+                  Sign up
+                </Link>
+              </Center>
+            </Text>
+          </Stack>
+        </Box>
+      </VStack>
+    </Center>
   );
 };
 
