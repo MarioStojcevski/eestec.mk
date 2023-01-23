5cf8f92cb475fe210a5088c71c769556771ed412
7zip <mane.sto@outlook.com>
2023-01-23 18:01:58 +0100

    FTR: created card variant

---
+++ frontend/styles/components/card-styles.ts
@@ -0,0 +1,15 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+
+export const TextStyles: ComponentStyleConfig = {
+  // style object for variants
+  variants: {
+    authCard: {
+      width: "800px",
+      height: "70vh",
+    },
+  },
+  // default values for 'size'
+  defaultProps: {
+    size: "",
+  },
+};
