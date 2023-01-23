c340e10b9f5cc2fdc04c63cbcb3754aa2aa0a770
Andrej Acevski <aacevski@gmail.com>
2023-01-23 18:24:14 +0100

    hotfix: fixing layout

--- frontend/components/layout/unauthenticated-layout.tsx
+++ frontend/components/layout/unauthenticated-layout.tsx
@@ -2,16 +2,16 @@ import { useRouter } from "next/router";
 import LandingPage from "../landing-page";
 import MissionVision from "../mission-vision";
 import Footer from "../shared/footer";
-import Header from "../shared/header";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const UnauthenticatedLayout = ({ children }: LayoutProps) => {
+  const { pathname } = useRouter();
+
   return (
     <>
-      <main>{children}</main>
       {pathname !== "/blog" && <LandingPage />}
       {pathname !== "/blog" && <MissionVision />}
       <main style={{ width: "80%", margin: "auto" }}>{children}</main>
