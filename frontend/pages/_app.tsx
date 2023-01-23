70b81e60251f3ed02975378a0b2d3856a5df5221
Andrej Acevski <aacevski@gmail.com>
2023-01-23 19:08:27 +0100

    hotfix: adding dynamic layouts

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,14 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { светописмо } from "../styles/светописмо";
-import UnauthenticatedLayout from "../components/layout/unauthenticated-layout";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <ChakraProvider resetCSS theme={светописмо}>
-      <UnauthenticatedLayout>
-        <Component {...pageProps} />
-      </UnauthenticatedLayout>
+      {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import { ReactElement } from "react";
 import BlogShowcase from "../components/blog-showcase";
+import LandingPage from "../components/landing-page";
+import UnauthenticatedLayout from "../components/layout/unauthenticated-layout";
 import MakingMemories from "../components/making-memories";
 import MeetTheBoard from "../components/meet-the-board";
+import MissionVision from "../components/mission-vision";
 import Partners from "../components/partners";
 import Statistics from "../components/statistics";
 import WhyYouShouldJoin from "../components/why-you-should-join";
@@ -8,19 +13,28 @@ import WhyYouShouldJoin from "../components/why-you-should-join";
 const Home = () => {
   return (
     <>
-      <Statistics />
+      <LandingPage />
 
-      <WhyYouShouldJoin />
+      <MissionVision />
+      <Box mx="auto" w="80%">
+        <Statistics />
 
-      <MakingMemories />
+        <WhyYouShouldJoin />
 
-      <BlogShowcase />
+        <MakingMemories />
 
-      <MeetTheBoard />
+        <BlogShowcase />
 
-      <Partners />
+        <MeetTheBoard />
+
+        <Partners />
+      </Box>
     </>
   );
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <UnauthenticatedLayout>{page}</UnauthenticatedLayout>;
 };
 
 export default Home;
