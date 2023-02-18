60d824eb98a7f100a199b320560be553941ed804
Petar Hristovski <petarhristovski23@gmail.com>
2023-02-18 11:26:45 +0100

    FIX: copy button size fixed. Static size.

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -10,10 +10,6 @@ import {
 import React, { useState } from "react";
 
 const Footer = () => {
-  /*
-  1.  from line 14-21 is a function with the purpose to change the text inside a button from "Copy" to "Copied!" 
-  as feedback to the user that they've copied the email address.
-  2.  In line 46-48, specifically line 47 -> We call the function*/
   const [buttonText, setButtonText] = useState("Copy");
   const handleOnClick = () => {
     navigator.clipboard.writeText("skopje.lc@gmail.com");
@@ -42,10 +38,10 @@ const Footer = () => {
             Rugjer Boshkovikj bb 1000 Skopje
           </Text>
           <HStack>
-            <Text variant="white" mr="2">
+            <Text variant="white" mr="6">
               skopje.lc@gmail.com
             </Text>
-            <Button size="xs" onClick={handleOnClick}>
+            <Button fontSize="12" width="14" height="6" onClick={handleOnClick}>
               {buttonText}
             </Button>
           </HStack>
