a502dfdf7d9468bbe908b260dadc59ec3c3b2327
Petar Hristovski <petarhristovski23@gmail.com>
2023-02-18 11:16:49 +0100

    FIX: iframe size changed, SVG deleted.

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -29,8 +29,8 @@ const Footer = () => {
         <Box>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d185.78867338759105!2d21.40655531520495!3d42.0051081466911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smk!4v1676655442519!5m2!1sen!2smk"
-            width="600"
-            height="300"
+            width="400"
+            height="200"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
